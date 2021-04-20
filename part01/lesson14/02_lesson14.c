d37508d2a6f7814c8a5f303432858786d2866af3
jandrzejewski1 <67760124+jandrzejewski1@users.noreply.github.com>
2021-04-20 20:56:31 +0200

    lesson14

--- part01/lesson14/02_lesson14.c
+++ part01/lesson14/02_lesson14.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(void)
-{
-    int tab[256];
-    int x = 0;
-    while (x != 256) {
-        printf("adres %d-go elementu %p\n", x, (void*)(&tab[x]));
-        x++;
+#ifdef __cplusplus
+extern "C" {
+    #endif
+    int main(void)
+    {
+        int tab[256];
+        int x = 0;
+        while (x != 256) {
+            printf("adres %d-go elementu %p\n", x, (void*)(&tab[x]));
+            x++;
+        }
+
+        return EXIT_SUCCESS;
     }
 
-    return EXIT_SUCCESS;
+    #ifdef __cplusplus
 }
+#endif
