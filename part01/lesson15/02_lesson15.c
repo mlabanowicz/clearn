8d82a9c1025aef093bc1d1b7c5589a3a87105cd4
jandrzejewski1 <67760124+jandrzejewski1@users.noreply.github.com>
2021-04-21 12:42:29 +0200

    lesson15

---
+++ part01/lesson15/02_lesson15.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#ifdef __cplusplus
+extern "C" {
+    #endif
+    int main(void)
+    {
+        int tablica [256];
+        int * p = &tablica[0];
+        int i=-1;
+        while(scanf("%d", p) == 1 && i != 255){
+            i++;
+            fprintf(stderr, "LOG:Wczytujemy znak %d do komórki o adresie %p i = %d!\n", tablica[i] ,p,i);
+            p++;
+        }
+        for(;i != 0;i--){
+            printf("%d\n",tablica[i]);
+        }
+        return EXIT_SUCCESS;
+    }
+    #ifdef __cplusplus
+}
+#endif
