cc1026675cb98345eed49867dd25b4d3dcd5f105
Maciej Labanowicz <M.Labanowicz@gmail.com>
2022-07-31 00:57:17 +0200

    lesson03_dynstruct/request.txt

--- part02/lesson02_datatype/data_type.c
+++ part02/lesson02_datatype/data_type.c
@@ -1,39 +1,43 @@
 #include "data_type.h"
 #include <limits.h>
 #include <stdio.h>
+#include <string.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-const char *data_type[7] = {"char", "short", "int", "long", "string", "float", "double"};
+
+/* static powodouje, ze tablica data_type nie bedzie dostepna z innych bibliotek */
+/* (gdybysmy chcieli, aby tablica byla dostepna, musiala by byc jej dekladaracja w pliku .h) */
+static const char *data_type[7] = {"char", "short", "int", "long", "string", "float", "double"};
 
 
 void datatype_info_char(char const *cptr) {
-    fprintf(stdout, "(%s) %ld:%ld@%p = %c\n", data_type[0], sizeof(*cptr), sizeof(*cptr) * 8, (void *) cptr, *cptr);
+    fprintf(stdout, "(%s) %ld:%ld@%p = %c\n", data_type[0], (long)sizeof(*cptr), (long)(sizeof(*cptr) * CHAR_BIT), (void *) cptr, *cptr);
 }
 
 void datatype_info_short(short const *hptr) {
-    fprintf(stdout, "(%s) %ld:%ld@%p = %d\n", data_type[1], sizeof(*hptr), sizeof(*hptr) * 8, (void *) hptr, *hptr);
+    fprintf(stdout, "(%s) %ld:%ld@%p = %hd\n", data_type[1], (long)sizeof(*hptr), (long)(sizeof(*hptr) * CHAR_BIT), (void *) hptr, *hptr);
 }
 
 void datatype_info_int(int const *iptr) {
-    fprintf(stdout, "(%s) %ld:%ld@%p = %d\n", data_type[2], sizeof(*iptr), sizeof(*iptr) * 8, (void *) iptr, *iptr);
+    fprintf(stdout, "(%s) %ld:%ld@%p = %d\n", data_type[2], (long)sizeof(*iptr), (long)(sizeof(*iptr) * CHAR_BIT), (void *) iptr, *iptr);
 }
 
 void datatype_info_long(long const *lptr) {
-    fprintf(stdout, "(%s) %ld:%ld@%p = %ld\n", data_type[3], sizeof(*lptr), sizeof(*lptr) * 8, (void *) lptr, *lptr);
+    fprintf(stdout, "(%s) %ld:%ld@%p = %ld\n", data_type[3], (long)sizeof(*lptr), (long)(sizeof(*lptr) * CHAR_BIT), (void *) lptr, *lptr);
 }
 
 void datatype_info_str(char const *str) {
-    fprintf(stdout, "(%s) %ld:%ld@%p = %s\n", data_type[4], sizeof(str), sizeof(str) * CHAR_BIT, (void *) str, str);
+    fprintf(stdout, "(%s) %ld:%ld@%p = %s\n", data_type[4], (long)strlen(str), (long)(sizeof(str) * CHAR_BIT), (void *) str, str);
 }
 
 void datatype_info_float(float const *fptr) {
-    fprintf(stdout, "(%s) %ld:%ld@%p = %f\n", data_type[5], sizeof(*fptr), sizeof(*fptr) * 8, (void *) fptr, *fptr);
+    fprintf(stdout, "(%s) %ld:%ld@%p = %f\n", data_type[5], (long)sizeof(*fptr), (long)(sizeof(*fptr) * CHAR_BIT), (void *) fptr, (double)(*fptr));
 }
 
 void datatype_info_double(double const *dptr) {
-    fprintf(stdout, "(%s) %ld:%ld@%p = %f\n", data_type[6], sizeof(*dptr), sizeof(*dptr) * 8, (void *) dptr, *dptr);
+    fprintf(stdout, "(%s) %ld:%ld@%p = %f\n", data_type[6], (long)sizeof(*dptr), (long)(sizeof(*dptr) * CHAR_BIT), (void *) dptr, *dptr);
 }
 
 #ifdef __cplusplus
---
+++ part02/lesson03_dynstruct/textdata.h
@@ -0,0 +1,36 @@
+
+#ifndef TEXTDATA_H__
+#define TEXTDATA_H__
+
+#include <stdio.h> /* FILE */
+
+#ifdef __cplusplus
+extern"C" {
+#endif
+
+typedef struct textdata_char_s textdata_char_t;
+
+struct textdata_char_s {
+  char              c;
+  textdata_char_t * next;
+};
+
+typedef struct textdata_line_s textdata_line_t;
+
+struct textdata_line_s {
+  textdata_char_t * data;
+  textdata_line_t * next;
+};
+
+/* creates text data structure in HEAP */
+textdata_line_t * TEXTDATA_LoadFile(FILE * f);
+
+/* destroy text data structure from HEAP */
+void TEXTDATA_Free(textdata_line_t * textdata);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TEXTDATA_H__ */
+
