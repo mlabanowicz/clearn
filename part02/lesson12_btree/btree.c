82d09df90003806ed56c37db8be70655b569ebbc
janandrzejewski <janandrzejewski@aol.com>
2023-01-20 21:07:27 +0100

    part02/lesson12_btree/btree.c update

--- part02/lesson12_btree/btree.c
+++ part02/lesson12_btree/btree.c
@@ -8,7 +8,7 @@ extern "C" {
 
 static int walk(int const * array, int height, int leaf)
 {
-    int index = (int)(pow(height+1,2)-pow(height,2) + leaf-1);
+    int index = (1<<(height)) + leaf;
     int sum=0;
     while(index != 0){
     sum += array[index];
@@ -25,11 +25,11 @@ static int walk(int const * array, int height, int leaf)
 
 int main(void)
 {
-  int btree [] = {0/*unused*/, 6, 2, 4, 2, 0, 1, 3 };
+  int btree [] = {0/*unused*/, 6, 2, 4, 2, 0, 1, 3 ,10 , 5, 9, 1, 13, 7, 8, 12};
 
   int r;
 
-  r = walk(btree, 2, 3);
+  r = walk(btree, 3, 7);
 
   printf("%d\n", r);
 
