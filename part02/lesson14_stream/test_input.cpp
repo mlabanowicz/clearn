f8454e954af1974fad4d96c9c920a4a02a515d69
Maciej Labanowicz <M.Labanowicz@adbglobal.com>
2023-02-13 14:36:47 +0100

    lesson14_stream/rating

---
+++ part02/lesson14_stream/test_input.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+
+int main()
+{
+  /* taki sobie program w C++ */
+
+  std::cout << '"' << '\'' << "Hello World /*to nie jest komentarz*/ // to tez nie {\"} :)\n"; // Komentarz
+
+  return 0;
+}
