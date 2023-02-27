718482dc1b585b91f68b4fb9701ca29c2b55313c
Maciej Labanowicz <M.Labanowicz@gmail.com>
2023-02-27 21:17:37 +0100

    part02/lesson14_stream/rating.txt + part02/lesson15_graph/request.txt

--- part02/lesson14_stream/test_input.cpp
+++ part02/lesson14_stream/test_input.cpp
@@ -5,7 +5,13 @@ int main()
 {
   /* taki sobie program w C++ */
 
+  /* komentarz // inny tekst
+   *
+   */
+
   std::cout << '"' << '\'' << "Hello World /*to nie jest komentarz*/ // to tez nie {\"} :)\n"; // Komentarz
+
+  std::cout << '\\' << std::endl;
 
   return 0;
 }
