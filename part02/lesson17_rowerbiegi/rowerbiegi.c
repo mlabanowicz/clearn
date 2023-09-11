b7e7b68151e033af3a287940b22ce310a55c9dbb
janandrzejewski <janandrzejewski@aol.com>
2023-09-11 22:08:53 +0200

    rowerbiegi.c update

--- part02/lesson17_rowerbiegi/rowerbiegi.c
+++ part02/lesson17_rowerbiegi/rowerbiegi.c
@@ -1,59 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
+typedef struct
+{
     int front_gear;
     int rear_gear;
 } GearCombination;
 
 int compareGearCombinations(const void *a, const void *b);
 
-int compareGearCombinations(const void *a, const void *b) {
-    GearCombination *comb1 = (GearCombination *)a;
-    GearCombination *comb2 = (GearCombination *)b;
-    
+int compareGearCombinations(const void *a, const void *b)
+{
+    GearCombination const *comb1 = (GearCombination *)a;
+    GearCombination const *comb2 = (GearCombination *)b;
+
     return (comb1->front_gear * comb2->rear_gear) - (comb2->front_gear * comb1->rear_gear);
 }
 
-int main(void) {
-    int N, M, i, j;
-    int rear_gear;
-    GearCombination *combinations;
-    if (scanf("%d", &N) != 1) {
+int main(void)
+{
+    int N, M, i, j, *front_gear, rear_gear;
+    GearCombination *combinations, previousCombination;
+    if (scanf("%d", &N) != 1)
+    {
         fprintf(stderr, "Failed to read integer.\n");
         return EXIT_FAILURE;
     }
-    
-    combinations = (GearCombination *)malloc(N * sizeof(GearCombination));
-    for (i = 0; i < N; i++) {
-        if (scanf("%d", &combinations[i].front_gear) != 1) {
+    front_gear = (int *)malloc(N * sizeof(int));
+    for (i = 0; i < N; i++)
+    {
+        if (scanf("%d", &front_gear[i]) != 1)
+        {
             fprintf(stderr, "Failed to read integer.\n");
             return EXIT_FAILURE;
         }
     }
-    
-    if (scanf("%d", &M) != 1) {
+    if (scanf("%d", &M) != 1)
+    {
         fprintf(stderr, "Failed to read integer.\n");
         return EXIT_FAILURE;
     }
-    
-    for (j = 0; j < M; j++) {
-        if (scanf("%d", &rear_gear) != 1) {
+    combinations = (GearCombination *)malloc(N * M * sizeof(GearCombination));
+    for (j = 0; j < M; j++)
+    {
+        if (scanf("%d", &rear_gear) != 1)
+        {
             fprintf(stderr, "Failed to read integer.\n");
             return EXIT_FAILURE;
         }
-        
-        for (i = 0; i < N; i++) {
-            combinations[i].rear_gear = rear_gear;
+        for (i = 0; i < N; i++)
+        {
+            combinations[N * j + i].front_gear = front_gear[i];
+            combinations[N * j + i].rear_gear = rear_gear;
         }
+    }
+    qsort(combinations, N * M, sizeof(GearCombination), compareGearCombinations);
 
-        qsort(combinations, N, sizeof(GearCombination), compareGearCombinations);
-        
-        for (i = 0; i < N; i++) {
+    previousCombination = combinations[0];
+    printf("%d/%d\n", previousCombination.front_gear, previousCombination.rear_gear);
+    for (i = 1; i < M * N; i++)
+    {
+        if (compareGearCombinations(&combinations[i], &previousCombination) != 0)
+        {
             printf("%d/%d\n", combinations[i].front_gear, combinations[i].rear_gear);
         }
+        previousCombination = combinations[i];
     }
-    
     free(combinations);
+    free(front_gear);
     return EXIT_SUCCESS;
 }
