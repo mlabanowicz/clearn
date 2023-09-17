10b1a1beb9a12078ba635ceb391e593f2cc5c6e4
janandrzejewski <janandrzejewski@aol.com>
2023-09-17 19:43:03 +0200

    bignum.c

---
+++ part02/lesson18_bignum/bignum.c
@@ -0,0 +1,60 @@
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdio.h>
+#include <limits.h>
+
+#define BITS (sizeof(unsigned short) * CHAR_BIT)
+#define BITS_HALF (BITS / 2)
+
+    unsigned short ushort_sum(unsigned short a, unsigned short b, unsigned short *carry_ptr);
+
+    unsigned short ushort_sum(unsigned short a, unsigned short b, unsigned short *carry_ptr)
+    {
+        unsigned short sumh, suml;
+        unsigned short bh = b >> BITS_HALF;
+        unsigned short bl = b & (((unsigned short)1 << BITS_HALF) - 1);
+        unsigned short ah = a >> BITS_HALF;
+        unsigned short al = a & (((unsigned short)1 << BITS_HALF) - 1);
+        sumh = ah + bh;
+        suml = al + bl;
+        if (suml > 0x00ff)
+        {
+            suml -= 0x0100;
+            sumh += 0x0001;
+        }
+        if (sumh > 0x00ff)
+        {
+            sumh -= 0x0100;
+            *carry_ptr = 0x0001;
+        }
+        else
+        {
+            *carry_ptr = 0x0000;
+        }
+        return (sumh << BITS_HALF) | suml;
+    }
+
+    int main(void)
+    {
+        unsigned int ia, ib;
+
+        while (scanf("0x%08x 0x%08x\n", &ia, &ib) == 2)
+        {
+            unsigned short a = (unsigned short)ia;
+            unsigned short b = (unsigned short)ib;
+
+            unsigned short carry;
+
+            unsigned sum = ushort_sum(a, b, &carry);
+
+            printf("0x%04x%04x\n", (unsigned)carry, (unsigned)sum);
+        }
+        return 0;
+    }
+
+#ifdef __cplusplus
+}
+#endif
