a6ad5ff3b857d69f5c86c31ddc1047303f7c9e03
Maciej Labanowicz <M.Labanowicz@adbglobal.com>
2024-04-29 13:44:07 +0200

    Updated memmory.c

--- part02/lesson21_memory/memory.c
+++ part02/lesson21_memory/memory.c
@@ -2,42 +2,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static int bar(int a, int b, int c, int * i)
+static int bar(int a, int b, int * c, int * i)
 {
-  if (*i)
+  if (0) { printf("%d %d %d %d\n", a, b, *c, *i); }
+
+  if (*i && ((a & 2) == 0))
   {
     --(*i);
-
-    if (!*i)
-    {
-      printf("%d\n", a + b + c);
-    }
+    ++(*c);
   }
 
   return *i;
 }
 
-static void foo(int a, int b, int c, int * i)
+static long foo(int a, int b, int * c, int * i)
 {
   /* [++] == kod do naprawienia == */
+
+  long result = 0;
 
   if (bar(a, b, c, i))
   {
     a += 5;
     b -= 7;
-    c += 3;
 
-    foo(a, b, c, i);
+    result = foo(a, b, c, i);
+
+    result += *i + *c;
   }
+
+  return result;
 
   /* [--] == kod do naprawienia == */
 }
 
 int main(void)
 {
-  int i = 1000 * 1000;
+  int i, c = 0;
 
-  foo(0, 0, 0, &i);
+  if (scanf("%d", &i) == 1)
+  {
+    printf("%ld\n", foo(0, 0, &c, &i));
+  }
 
   return EXIT_SUCCESS;
 }
