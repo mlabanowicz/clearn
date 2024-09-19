3ac5f00bd972c7006de475079a96588628eba56d
Maciej Labanowicz <M.Labanowicz@adbglobal.com>
2024-09-19 17:01:04 +0200

    {A} _/memory2.c {A} _/rating.txt

---
+++ part02/lesson21_memory/memory2.c
@@ -0,0 +1,51 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+static int bar(int a, int b, int * c, int * i)
+{
+  if (0) { printf("%d %d %d %d\n", a, b, *c, *i); }
+
+  if (*i && ((a & 2) == 0))
+  {
+    --(*i);
+    ++(*c);
+  }
+
+  return *i;
+}
+
+static long foo(int a, int b, int * c, int * i)
+{
+  /* [++] == kod do naprawienia == */
+
+  long result = 0;
+
+  if (bar(a, b, c, i))
+  {
+    a += 5;
+    b -= 7;
+
+    result = foo(a, b, c, i);
+
+    result += *i + *c;
+
+    bar(a, b, c, i);
+  }
+
+  return result;
+
+  /* [--] == kod do naprawienia == */
+}
+
+int main(void)
+{
+  int i, c = 0;
+
+  if (scanf("%d", &i) == 1)
+  {
+    printf("%ld\n", foo(0, 0, &c, &i));
+  }
+
+  return EXIT_SUCCESS;
+}
